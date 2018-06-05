8b4f09ede9663fc26f2426b9632de48b087f3524
Hemant Sethi <sethi.hemant@gmail.com>
2018-06-05 09:43:32 -0400

    Updated Solution using O(1) space and O(N) single pass using 2 pointer approach.

--- src/main/java/com/leetcode/hard/TrappingRainWater.java
+++ src/main/java/com/leetcode/hard/TrappingRainWater.java
@@ -14,24 +14,28 @@ public class TrappingRainWater {
 	}
 	
 	  public int trap(int[] height) {
-		  // Instead of calculating the max_left and max_right for each array index everytime, lets just precompute that using 2 aux arrays in 1 pass. O(N). 
-		  // Now the same approach works in O(N) time and O(N) aux space. For each entry in the array, calculate the amount of water it traps by finding the Max bar length(greater than itself) on either
-		  // side, and the water it can hold is minimum of those minus its current height. 
 		  if(height == null || height.length <3) return 0;
 		  int sum = 0;
-		  Stack<Integer> st = new Stack<>();
-		  int current = 0;
-		  while(current < height.length) {
-			  while(!st.isEmpty() && height[current] > height[st.peek()]) {
-				  int top = st.pop();
-				  if(st.isEmpty()) // Nothing to bound the above element.
-					  break;
-				  int distance = current-st.peek()-1;
-				  int bounded_height = Math.min(height[current], height[st.peek()]) - height[top];
-				  sum += distance * bounded_height;
+		  int left = 0, right = height.length-1;
+		  int leftmax = 0, rightmax = 0;
+
+		  while(left < right) {
+			  if(height[left] < height[right]) {
+				  if(height[left] > leftmax)
+					  leftmax = height[left];
+				  else 
+					  sum += leftmax - height[left];
+				  ++left;
 			  }
-			  st.push(current++);
+			  else {
+				  if(height[right] > rightmax)
+					  rightmax = height[right];
+				  else 
+					  sum += rightmax - height[right];
+				  --right;
+			  }
 		  }
+		  
 		  return sum;
 	    }
 
