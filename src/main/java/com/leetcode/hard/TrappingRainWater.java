8decf61166460e63e291ee88497d1fb4bef83c4b
Hemant Sethi <sethi.hemant@gmail.com>
2018-06-05 07:46:02 -0400

    Added an O(N^2) brute force solution.

--- src/main/java/com/leetcode/hard/TrappingRainWater.java
+++ src/main/java/com/leetcode/hard/TrappingRainWater.java
@@ -12,27 +12,41 @@ public class TrappingRainWater {
 	}
 	
 	  public int trap(int[] height) {
-	        int sum = 0;
-	        if(height == null || height.length < 3)
-	            return sum;
-	     
-	        for(int i = 0 ; i < height.length-2; i++){
-	            if(height[i] == 0) continue;
-	        	int candidate = height[i];
-	            int potentialSum = 0;
-	            int j = i+1;
-	            while(j < height.length && height[j] < candidate){
-	                potentialSum += candidate-height[j];
-	                j++;
-	            }
-	            if(j < height.length && height[j] >= candidate){
-	                sum += potentialSum;
-	                i = j-1;
-	            }
-	            //else
-	             //   i++;
-	        }
-	        return sum;
+		    int sum = 0;
+		  //  int size = height.length;
+		    for (int i = 1; i < height.length - 1; i++) {
+		        int max_left = 0, max_right = 0;
+		        for (int j = i; j >= 0; j--) { //Search the left part for max bar size
+		            max_left = Math.max(max_left, height[j]);
+		        }
+		        for (int j = i; j < height.length ; j++) { //Search the right part for max bar size
+		            max_right = Math.max(max_right, height[j]);
+		        }
+		        sum += Math.min(max_left, max_right) - height[i];
+		    }
+		    return sum;
+			  // My initial solution which Didn't take into account few cases, thus failed.
+		    /*	        int sum = 0;
+		    	        if(height == null || height.length < 3)
+		    	            return sum;
+		    	     
+		    	        for(int i = 0 ; i < height.length-2; i++){
+		    	            if(height[i] == 0) continue;
+		    	        	int candidate = height[i];
+		    	            int potentialSum = 0;
+		    	            int j = i+1;
+		    	            while(j < height.length && height[j] < candidate){
+		    	                potentialSum += candidate-height[j];
+		    	                j++;
+		    	            }
+		    	            if(j < height.length && height[j] >= candidate){
+		    	                sum += potentialSum;
+		    	                i = j-1;
+		    	            }
+		    	            //else
+		    	             //   i++;
+		    	        }
+		    	        return sum;*/
 	    }
 
 }
