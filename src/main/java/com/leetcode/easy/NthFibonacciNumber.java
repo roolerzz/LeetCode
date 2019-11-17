3b26e895447c684d2996e8bead1843c7cd4aecf2
Hemant Sethi <sethi.hemant@gmail.com>
2019-11-17 08:07:22 -0800

    Adding Fibonacci number both recursive and iterative using memoization.

---
+++ src/main/java/com/leetcode/easy/NthFibonacciNumber.java
@@ -0,0 +1,51 @@
+package com.leetcode.easy;
+
+import java.util.Arrays;
+
+//https://leetcode.com/problems/fibonacci-number/
+public class NthFibonacciNumber {
+
+    public int nthFiboRec(int n) {
+        validate(n);
+        int[] fibo = new int[n+1];
+        Arrays.fill(fibo, -1);
+        fibo[0] =0;
+        fibo[1]=1;
+        return nthFiboRec(n, fibo);
+    }
+
+    private void validate(int n) {
+        if (n < 0) throw new IllegalArgumentException();
+    }
+
+    private int nthFiboRec(int n, int[] fibo) {
+        if(alreadyComputed(n, fibo)) return fibo[n];
+        int result = nthFiboRec(n-1, fibo) + nthFiboRec(n-2,fibo);
+        fibo[n]= result;
+        return fibo[n];
+    }
+
+    private boolean alreadyComputed(int n, int[] fibo){
+        return fibo[n] != -1;
+    }
+
+    public int nthFiboIterative(int n) {
+        validate(n);
+        int[] fibo = new int[n+1];
+        fibo[0]=0;
+        fibo[1]=1;
+        for(int i = 2; i <= n ;i++){
+            fibo[i] = fibo[i-2] + fibo[i-1];
+        }
+        return fibo[n];
+    }
+
+
+  public static void main(String[] args) {
+        NthFibonacciNumber fibObj = new NthFibonacciNumber();
+        int recN = 6;
+        int iterN = 6;
+    System.out.println("Recursive calculating fibo "+ recN + " : "  + fibObj.nthFiboRec(recN));
+    System.out.println("Iterative calculating fibo "+ iterN + " : "  + fibObj.nthFiboRec(iterN));
+  }
+}
