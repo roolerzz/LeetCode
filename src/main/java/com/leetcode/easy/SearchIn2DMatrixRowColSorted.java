def81d918ddf71db1b38acf88d05fc9f588e88c3
Hemant Sethi <sethi.hemant@gmail.com>
2019-11-19 16:54:37 -0800

    Added documentation/explanation for the algorithm and various approaches.

--- src/main/java/com/leetcode/easy/SearchIn2DMatrixRowColSorted.java
+++ src/main/java/com/leetcode/easy/SearchIn2DMatrixRowColSorted.java
@@ -17,6 +17,18 @@ public class SearchIn2DMatrixRowColSorted {
 
     }
 
+    // Various approaches:
+    // 1. Brute force - O(M*N) Search through every element in the matrix. Doesn't utilizes any information given.
+    // 2. Binary search over each row. O(M*log(N)). Utilizes the fact that elements are sorted within each row. Still doesn't utilizes
+    // more information that they are also sorted column wise.
+    // 3. Looking at an element at index(i,j) what can you tell?? Well, element on left and above it are smaller and ones to
+    // the right and lower are larger. Looking at element if we can compare it with the one we are looking for, do we know, which
+    // direction should we move. Probaby not because for each case, we have 2 options. Except if we start from specific directions.
+    // Consider the corner elements of the matrix. Well, for 0,0 if you get elements bigger, it could be on either side. For n,n, if
+    // you get element smaller it could be on the either side. Interesting corner elements are N,0 and 0,N. e.g. for 0,N,
+    // If you compare the searching elements with 0,N it could be either greater or smaller. If elem we are looking for is smaller,
+    // we can go left(basically discard all the elements below it) or if greater, we can go below(discard all the elements to the left of it on that row).
+    // In the worst case, we would be at the opposite corner. Worst case time complexity would be O(M+N).
     public Pair findElement(int[][] mat, int elem) {
         if (mat == null) return null;
         int currX = mat[0].length-1;
